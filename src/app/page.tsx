6c9e47913992b14a4bebc0ea1a1f3b32eb6ef2d3
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-11-10 23:48:43 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import About from "./components/about";
+import Contact from "./components/contact";
+import Hero from "./components/hero";
+import Project from "./components/project";
+import Skill from "./components/skill";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Skill />
+      <Project />
+      <Contact />
+    </div>
+  );
+}
